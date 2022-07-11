252300dd12d506320c961fd83bb51512d807201e
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-07-11 15:05:53 +0300

    add function calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,77 +1,61 @@
-const calc = () => {
+import { animate } from "./helpers";
+
+const calc = (price = 1000) => {
+    console.log('123');
+
     const calcType = document.getElementById("calc-type");
     const calcArea = document.getElementById("calc-input");
     const calcMaterial = document.getElementById("calc-type-material");
-    const total = document.getElementById("calc-total");
+    const calcTotal = document.getElementById("calc-total");
     const calcBlock = document.getElementById("calc");
 
-    console.log(calcBlock);
-    console.log(calcType);
-    console.log(calcMaterial);
-    console.log(calcArea);
-    console.log(total);
 
-    // const textCalcBlock = document.querySelectorAll('.calc-block input');
-    // const calcBlock = document.querySelector('.calc-block');
-    // const calcType = document.querySelector('.calc-type');
-    // const calcSquare = document.querySelector('.calc-square');
-    // const calcCount = document.querySelector('.calc-count');
-    // let calcDay = document.querySelector('.calc-day');
-    // const total = document.getElementById('total');
+    const countCalc = () => {
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+        const calcSquareValue = calcArea.value;
+        let calcMaterialValue = +calcMaterial.options[calcMaterial.selectedIndex].value;
 
-    // const countCalc = () => {
-    //     const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
-    //     const calcSquareValue = calcSquare.value;
+        let totalValue = 0;
 
-    //     let totalValue = 0;
-    //     let calcCountValue = 1;
-    //     let calcDayValue = 1;
 
-    //     if (calcCount.value > 1) {
-    //         calcCountValue += +calcCount.value / 10;
-    //     }
+        if (calcTypeValue && calcArea.value && calcMaterialValue) {
+            totalValue = price * calcSquareValue * calcTypeValue * calcMaterialValue;
+        } else {
+            totalValue = 0;
+        }
 
-    //     if (calcDay.value && calcDay.value < 5) {
-    //         calcDayValue = 2;
-    //     } else if (calcDay.value && calcDay.value < 10) {
-    //         calcDayValue = 1.5;
-    //     }
 
-    //     if (calcTypeValue && calcSquareValue) {
-    //         totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+        animate({
+            duration: 300,
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                calcTotal.textContent = Math.floor(totalValue * progress);
+                calcTotal.setAttribute(
+                    "placeholder",
+                    Math.floor(totalValue * progress)
+                );
+            },
+        });
+    };
 
-    //     } else {
-    //         totalValue = 0;
-    //     }
-    //     animate({
-    //         duration: 500,
-    //         timing(timeFraction) {
-    //             return timeFraction;
-    //         },
-    //         draw(progress) {
-    //             for (let i = 0; i <= totalValue; i++) {
-    //                 total.textContent = Math.floor(i * progress);
-    //             }
-    //         }
-    //     });
-    // };
+    if (calcBlock) {
+        calcBlock.addEventListener("input", (e) => {
+            if (
+                e.target === calcType ||
+                e.target === calcArea ||
+                e.target === calcMaterial
+            ) {
+                countCalc();
+            }
+        });
 
-    // calcBlock.addEventListener('input', (e) => {
-    //     if (e.target === calcType ||
-    //         e.target === calcSquare ||
-    //         e.target === calcCount ||
-    //         e.target === calcDay) {
-    //         countCalc();
-    //     }
 
-    // });
-
-    // textCalcBlock.forEach(textBlock => {
-    //     textBlock.addEventListener('input', (e) => {
-    //         e.target.value = e.target.value.replace(/\D+/gi, '');
-    //     });
-
-    // });
+        calcArea.addEventListener("input", (e) => {
+            e.target.value = e.target.value.replace(/[^\d]+/, "");
+        });
+    }
 };
 
 export default calc;
